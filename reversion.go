ffccd6d0dd3c3cffba1fa3b8b4830f2157c8c486
sevendollar <sevendollar@gmail.com>
2021-04-08 11:50:43 +0800

    test

--- reversion.go
+++ reversion.go
@@ -1,12 +1,12 @@
 package jef
 
-func ReverseStringSlice(ss []string) {
+func reverseStringSlice(ss []string) {
 	for i, j := 0, len(ss)-1; j > i; i, j = i+1, j-1 {
 		ss[i], ss[j] = ss[j], ss[i]
 	}
 }
 
-func ReverseIntSlice(si []int) {
+func reverseIntSlice(si []int) {
 	for i, j := 0, len(si)-1; j > i; i, j = i+1, j-1 {
 		si[i], si[j] = si[j], si[i]
 	}
